49eb716968b597cb41d6a8f0d599db4acbc5862e
rakeshkumar4u <rakesh848114@gmail.com>
2025-05-10 22:58:32 +0530

    Initial Commit

---
+++ claim-management/src/app/model/surveyorfees.spec.ts
@@ -0,0 +1,7 @@
+import { SurveyorFees } from './surveyorfees';
+
+describe('Surveyorfees', () => {
+  it('should create an instance', () => {
+    expect(new SurveyorFees()).toBeTruthy();
+  });
+});
